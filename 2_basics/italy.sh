4212165fb2fc568118b0115ad5a2173f57ce351b
Max Jones <meghanj@alum.mit.edu>
2022-07-05 13:42:34 -0400

    Use ? for -J size in inset

--- 2_basics/italy.sh
+++ 2_basics/italy.sh
@@ -6,6 +6,6 @@ gmt begin italy pdf,png # Starting our new gmt modern mode session, calling plot
     gmt coast -R5/20/35/50 -Wthin -Gwheat -EIT+gred -Df -Sazure -B -N1/thick,red -JM15c
     # Show where Italy is in the world via a map inset
     gmt inset begin -DjTR+w4c+o0.2c -F+gwhite+pthick
-        gmt coast -Rg -JG10E/25N/4c -Gwheat -Bg -EIT+gred
+        gmt coast -Rg -JG10E/25N/? -Gwheat -Bg -EIT+gred
     gmt inset end
 gmt end show # end will finish the plots and open them in a viewer

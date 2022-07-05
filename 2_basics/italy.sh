3f55c05b6e5a40f4cf99a22577d6c85639c4e439
Max Jones <meghanj@alum.mit.edu>
2022-07-04 22:17:53 -0400

    Remove deprecated inset option

--- 2_basics/italy.sh
+++ 2_basics/italy.sh
@@ -5,7 +5,7 @@ gmt begin italy pdf,png # Starting our new gmt modern mode session, calling plot
     # Lay down painted continent with national borders on a Mercator map
     gmt coast -R5/20/35/50 -Wthin -Gwheat -EIT+gred -Df -Sazure -B -N1/thick,red -JM15c
     # Show where Italy is in the world via a map inset
-    gmt inset begin -DjTR+w4c+o0.2c -M0 -F+gwhite+pthick
+    gmt inset begin -DjTR+w4c+o0.2c -F+gwhite+pthick
         gmt coast -Rg -JG10E/25N/4c -Gwheat -Bg -EIT+gred
     gmt inset end
 gmt end show # end will finish the plots and open them in a viewer

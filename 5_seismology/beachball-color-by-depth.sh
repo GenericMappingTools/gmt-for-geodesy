d36386d29a6d6247f6aed0f68ce0052bec29364b
Max Jones <14077947+maxrjones@users.noreply.github.com>
2023-06-19 19:48:00 -0400

    Update seismology

--- 5_seismology/beachball-color-by-depth.sh
+++ 5_seismology/beachball-color-by-depth.sh
@@ -5,8 +5,8 @@
 #
 gmt begin beachball-color-by-depth png
     gmt basemap -JM15c -R110/120/30/35 -Baf
-    # create a CPT from the seis master CPT, for earthquake depth from 0 to 50 km
-    gmt makecpt -Cseis -T0/50
+    # create a CPT from the batlow CPT, for earthquake depth from 0 to 50 km
+    gmt makecpt -Cbatlow -T0/50
     # -C: let compressive part color be determined by the z-value in the third column.
     gmt meca meca.dat -Sa1c -C
     # Plot the colorbar in the Middle Right (MR) and set the label using -Bxaf+l
